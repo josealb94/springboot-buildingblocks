6a78027432d49f7748b55db17b317984e27e0172
Jose Gallegos <jgallegos@emagic.fin.ec>
2019-10-02 13:36:50 -0500

    i18n - First Commit

--- src/main/java/com/josegallegos/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/josegallegos/restservices/SpringbootBuildingblocksApplication.java
@@ -1,14 +1,32 @@
 package com.josegallegos.restservices;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
 	public static void main(String[] args) {
-		//Codigo Principal
 		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localResolver() {
+		AcceptHeaderLocaleResolver localResolver = new AcceptHeaderLocaleResolver();
+		localResolver.setDefaultLocale(Locale.US);
+		return localResolver;
+	}
+	
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
 }
